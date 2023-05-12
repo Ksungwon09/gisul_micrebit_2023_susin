66deb7d1148c52cd2932a5ee41449cde47520d04
Ksungwon09 <129531909+Ksungwon09@users.noreply.github.com>
2023-05-12 13:34:22 +0900

    기초작업

--- main.ts
+++ main.ts
@@ -1,4 +1,31 @@
+function 쫘회전 () {
+	
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        젅니()
+    } else if (receivedNumber == 1) {
+        훚니()
+    } else if (receivedNumber == 2) {
+        쫘회전()
+    } else if (receivedNumber == 3) {
+        웃회전()
+    }
+})
+function 훚니 () {
+	
+}
+function 웃회전 () {
+	
+}
+function 젅니 () {
+	
+}
 radio.setGroup(122)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P12, 0)
+pins.digitalWritePin(DigitalPin.P13, 0)
 basic.showLeds(`
     . . # . .
     . . # . .
