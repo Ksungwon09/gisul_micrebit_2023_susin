330acf58e7ce0e9f0611dbcc66f6f738528fd3b1
Ksungwon09 <129531909+Ksungwon09@users.noreply.github.com>
2023-05-22 14:42:15 +0900

    버그수정

--- main.ts
+++ main.ts
@@ -3,8 +3,8 @@ function 쫘회전 () {
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.analogWritePin(AnalogPin.P2, 300)
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     control.waitMicros(500)
 }
 function 고속쫘회전2 () {
@@ -12,11 +12,11 @@ function 고속쫘회전2 () {
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.analogWritePin(AnalogPin.P2, 800)
-    pins.digitalWritePin(DigitalPin.P12, 0)
-    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
     control.waitMicros(500)
 }
-radio.onReceivedNumber(function (receivedNumber) {
+radio.onReceivedNumberDeprecated(function (receivedNumber) {
     if (receivedNumber == 0) {
         젅니()
         basic.showNumber(0)
@@ -38,8 +38,6 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 6) {
         고속쫘회전2()
         basic.showNumber(6)
-    } else {
-    	
     }
 })
 function 훚니 () {
@@ -47,8 +45,8 @@ function 훚니 () {
     pins.digitalWritePin(DigitalPin.P12, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.analogWritePin(AnalogPin.P2, 800)
-    pins.digitalWritePin(DigitalPin.P12, 0)
-    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
     control.waitMicros(500)
 }
 function 웃회전 () {
@@ -56,8 +54,8 @@ function 웃회전 () {
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.analogWritePin(AnalogPin.P2, 800)
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     control.waitMicros(500)
 }
 function 젅니 () {
@@ -65,8 +63,8 @@ function 젅니 () {
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.analogWritePin(AnalogPin.P2, 800)
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     control.waitMicros(500)
 }
 function 고속웃회전 () {
@@ -74,8 +72,8 @@ function 고속웃회전 () {
     pins.digitalWritePin(DigitalPin.P12, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.analogWritePin(AnalogPin.P2, 800)
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     control.waitMicros(500)
 }
 function 젓지 () {
@@ -83,8 +81,8 @@ function 젓지 () {
     pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.analogWritePin(AnalogPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     control.waitMicros(500)
 }
 radio.setGroup(122)
